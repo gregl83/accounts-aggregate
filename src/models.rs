d347cd3774be010409887bda0f3ca99ac2e37b0e
Gregory Langlais <general@gregorylanglais.com>
2021-02-10 12:55:57 -0500

    add duplicate command check
    - do not process commands that same event in history (type + tx)

--- src/models.rs
+++ src/models.rs
@@ -26,7 +26,7 @@ pub enum CommandType {
     Chargeback,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub enum Event {
     Credited { tx: TransactionId, amount: Currency },
     Debited { tx: TransactionId, amount: Currency },
@@ -62,8 +62,11 @@ impl Account {
         }
     }
 
+    fn has_event(&self, event: &Event) -> bool {
+        self.events.iter().any(|e| { e == event })
+    }
+
     fn find_genesis_amount(&self, key: TransactionId) -> Option<Currency> {
-        println!("{:?}", self.events);
         let mut transaction_amount: Option<Currency> = None;
         for event in &self.events {
             if let Event::Credited { tx, amount } = event {
@@ -136,7 +139,11 @@ impl Account {
                 if amount.is_none() {
                     bail!("amount is none for deposit account({}) transaction({})", command.client, command.tx);
                 }
-                vec![Event::Credited {tx: command.tx, amount: amount.unwrap()}]
+                let event = Event::Credited {tx: command.tx, amount: amount.unwrap()};
+                if self.has_event(&event) {
+                    bail!("duplicate deposit account({}) transaction({})", command.client, command.tx);
+                }
+                vec![event]
             }
             CommandType::Withdraw => {
                 let amount = command.amount;
@@ -144,34 +151,175 @@ impl Account {
                     bail!("amount is none for withdraw account({}) transaction({})", command.client, command.tx);
                 }
                 let amount_value = amount.unwrap();
+                let event = Event::Debited {tx: command.tx, amount: amount_value};
+                if self.has_event(&event) {
+                    bail!("duplicate withdraw account({}) transaction({})", command.client, command.tx);
+                }
                 if amount_value > self.available {
                     bail!("amount({}) exceeds available({}) withdraw account({}) transaction({})", amount_value, self.available, command.client, command.tx);
                 }
-                vec![Event::Debited {tx: command.tx, amount: amount_value}]
+                vec![event]
             }
             CommandType::Dispute => {
                 let amount = self.find_genesis_amount(command.tx);
                 if amount.is_none() {
                     bail!("unable to find account({}) transaction({}) to dispute", command.client, command.tx);
                 }
-                vec![Event::Held {tx: command.tx, amount: amount.unwrap()}]
+                let event = Event::Held {tx: command.tx, amount: amount.unwrap()};
+                if self.has_event(&event) {
+                    bail!("duplicate dispute account({}) transaction({})", command.client, command.tx);
+                }
+                vec![event]
             }
             CommandType::Resolve => {
                 let amount = self.find_dispute_amount(command.tx);
                 if amount.is_none() {
                     bail!("unable to find disputed account({}) transaction({}) to resolve", command.client, command.tx);
                 }
-                vec![Event::Released {tx: command.tx, amount: amount.unwrap()}]
+                let event = Event::Released {tx: command.tx, amount: amount.unwrap()};
+                if self.has_event(&event) {
+                    bail!("duplicate resolve account({}) transaction({})", command.client, command.tx);
+                }
+                vec![event]
             }
             CommandType::Chargeback => {
                 let amount = self.find_dispute_amount(command.tx);
                 if amount.is_none() {
                     bail!("unable to find disputed account({}) transaction({}) to chargeback", command.client, command.tx);
                 }
-                vec![Event::Reversed {tx: command.tx, amount: amount.unwrap()}, Event::Locked]
+                let event = Event::Reversed {tx: command.tx, amount: amount.unwrap()};
+                if self.has_event(&event) {
+                    bail!("duplicate chargeback account({}) transaction({})", command.client, command.tx);
+                }
+                vec![event, Event::Locked]
             }
         };
 
         Ok(events)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn deposit_accepted() {
+        let client = 1;
+        let tx = 10;
+
+        let mut account = Account::new(client);
+        let command = Command {
+            name: CommandType::Deposit,
+            client,
+            tx,
+            amount: Some(Decimal::new(990000, 4))
+        };
+        let events = account.handle(command).unwrap();
+        account.apply(events);
+
+        assert_eq!(account.version, 1);
+        assert_eq!(account.client, client);
+        assert_eq!(account.available, Decimal::new(990000, 4));
+        assert_eq!(account.held, Decimal::new(0, 4));
+        assert_eq!(account.total, Decimal::new(990000, 4));
+        assert!(!account.locked);
+        assert_eq!(account.events.len(), 1);
+    }
+
+    #[test]
+    fn deposit_declined_duplicate() {
+        let client = 1;
+        let tx = 10;
+
+        let mut account = Account::new(client);
+        let command = Command {
+            name: CommandType::Deposit,
+            client,
+            tx,
+            amount: Some(Decimal::new(990000, 4))
+        };
+        let events = account.handle(command).unwrap();
+        account.apply(events);
+        let command = Command {
+            name: CommandType::Deposit,
+            client,
+            tx,
+            amount: Some(Decimal::new(990000, 4))
+        };
+        let events = account.handle(command);
+
+        assert!(events.is_err());
+        assert_eq!(account.version, 1);
+        assert_eq!(account.client, client);
+        assert_eq!(account.available, Decimal::new(990000, 4));
+        assert_eq!(account.held, Decimal::new(0, 4));
+        assert_eq!(account.total, Decimal::new(990000, 4));
+        assert!(!account.locked);
+        assert_eq!(account.events.len(), 1);
+    }
+
+    #[test]
+    fn withdraw_accepted() {
+        let client = 1;
+        let tx = 10;
+
+        let mut account = Account::new(client);
+        let command = Command {
+            name: CommandType::Deposit,
+            client,
+            tx,
+            amount: Some(Decimal::new(990000, 4))
+        };
+        let events = account.handle(command).unwrap();
+        account.apply(events);
+        let command = Command {
+            name: CommandType::Withdraw,
+            client,
+            tx,
+            amount: Some(Decimal::new(980000, 4))
+        };
+        let events = account.handle(command).unwrap();
+        account.apply(events);
+
+        assert_eq!(account.version, 2);
+        assert_eq!(account.client, client);
+        assert_eq!(account.available, Decimal::new(10000, 4));
+        assert_eq!(account.held, Decimal::new(0, 4));
+        assert_eq!(account.total, Decimal::new(10000, 4));
+        assert!(!account.locked);
+        assert_eq!(account.events.len(), 2);
+    }
+
+    #[test]
+    fn withdraw_declined_insufficient_balance() {
+        let client = 1;
+        let tx = 10;
+
+        let mut account = Account::new(client);
+        let command = Command {
+            name: CommandType::Deposit,
+            client,
+            tx,
+            amount: Some(Decimal::new(990000, 4))
+        };
+        let events = account.handle(command).unwrap();
+        account.apply(events);
+        let command = Command {
+            name: CommandType::Withdraw,
+            client,
+            tx,
+            amount: Some(Decimal::new(1000000, 4))
+        };
+        let events = account.handle(command);
+
+        assert!(events.is_err());
+        assert_eq!(account.version, 1);
+        assert_eq!(account.client, client);
+        assert_eq!(account.available, Decimal::new(990000, 4));
+        assert_eq!(account.held, Decimal::new(0, 4));
+        assert_eq!(account.total, Decimal::new(990000, 4));
+        assert!(!account.locked);
+        assert_eq!(account.events.len(), 1);
     }
 }
